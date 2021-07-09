6f4eb9974dd2e765539f8e6e1dcfe3452521d443
Marco Bresciani <marco.bresciani@gucci.com>
2021-07-09 14:49:08 +0200

    Add nicer margins for "turn"
    A bit more visible with respect to normal text.

--- components/Separator.tsx
+++ components/Separator.tsx
@@ -4,17 +4,20 @@ import styled from 'styled-components';
 
 const Container = styled(View)`
         flex-direction: row;
+        margin: 10px 0;
 `;
 
 const Line = styled(View)`
         background-color: black;
         height: 1px;
         flex: 1;
+        margin: 3px 0 0 0;
         align-self: center;
 `;
 
 const Content = styled(Text)`
         align-self: center;
+        font-style: italic;
         padding-left: 5px;
         padding-right: 5px;
 `;

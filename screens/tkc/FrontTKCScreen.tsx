db3807f11baa86632c798e9f9110b332114de849
Marco Bresciani <marco.bresciani@gucci.com>
2022-01-05 10:57:41 +0100

    Improve copyright notice

--- screens/tkc/FrontTKCScreen.tsx
+++ screens/tkc/FrontTKCScreen.tsx
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2021-2022 Marco Bresciani
+ * © 2021-2022 Marco Bresciani
  * This file is part of TKCompanionApp.
  *
  * TKCompanionApp is free software: you can redistribute it and/or
@@ -9,11 +9,12 @@
  *
  * TKCompanionApp is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ * along with TKCompanionApp.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
  * SPDX-FileCopyrightText: 2021-2022 Marco Bresciani
  *

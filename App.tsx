634699b0be79658589696651c26ef826b4ff3d63
Marco Bresciani <marco.bresciani@gucci.com>
2022-02-15 08:32:30 +0100

    Add I18n basics and icons
    Might work, on my machine.

--- App.tsx
+++ App.tsx
@@ -1,115 +1,195 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
+/*
+ * © 2021-2022 Marco Bresciani
+ * This file is part of TKCompanionApp.
  *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
+ * TKCompanionApp is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * @format
+ * TKCompanionApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TKCompanionApp.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-FileCopyrightText: 2021-2022 Marco Bresciani
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 
-import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+// import AboutScreen from "./screens/about/AboutScreen";
+import Icon from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/dist/glyphmaps/Ionicons.json';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import KTSScreen from "./screens/KTSScreen";
+// import HomeScreen from "./screens/HomeScreen";
+// import FiveQCard from "./screens/5qcard/5QScreen";
+// import TkcScreen from "./screens/tkc/TkcScreen";
+import i18next from 'i18next';
+import {tEn, tItIT} from './i18n';
+import {Text, View} from 'react-native';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+import {Platform, NativeModules} from 'react-native';
+
+// const locale =
+//         Platform.OS === 'ios'
+//                 ? NativeModules.SettingsManager.settings
+//                           .AppleLanguages[0] ||
+//                   NativeModules.SettingsManager.settings.AppleLocale
+//                 : NativeModules.I18nManager.localeIdentifier;
+
+type IoniconsIconNames = keyof typeof Ionicons;
+
+const resources = {
+        en: {
+                translation: tEn,
+        },
+        'it-IT': {
+                translation: tItIT,
+        },
 };
 
-const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+i18next.init({
+        cleanCode: true,
+        compatibilityJSON: 'v3',
+        debug: false,
+        interpolation: {
+                escapeValue: false,
+        },
+        fallbackLng: 'en',
+        lng: 'it-IT',
+        nonExplicitSupportedLngs: true,
+        resources: resources,
 });
+
+const Tab = createMaterialBottomTabNavigator();
+
+const TabIcon5Q = ({color}: {color: string}) => (
+        <Icon
+                style={{fontFamily: 'OpenSans', fontSize: 16}}
+                name="people-circle-outline"
+                color={color}
+                size={16}
+        />
+);
+
+const TabIconAbout = ({color}: {color: string}) => (
+        <Icon
+                style={{fontFamily: 'OpenSans', fontSize: 16}}
+                name="information-circle-outline"
+                color={color}
+                size={16}
+        />
+);
+
+const TabIconHome = ({color}: {color: string}) => (
+        <Icon
+                style={{fontFamily: 'OpenSans', fontSize: 16}}
+                name="home"
+                color={color}
+                size={16}
+        />
+);
+
+const TabIconKTS = ({color}: {color: string}) => (
+        <Icon
+                style={{fontFamily: 'OpenSans', fontSize: 16}}
+                name="warning-outline"
+                color={color}
+                size={16}
+        />
+);
+
+const TabIconTKC = ({color}: {color: string}) => (
+        <Icon
+                style={{fontFamily: 'OpenSans', fontSize: 16}}
+                name="book-outline"
+                color={color}
+                size={16}
+        />
+);
+
+function HomeScreen() {
+        return (
+                <View
+                        style={{
+                                flex: 1,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                        }}>
+                        <Text>Home!</Text>
+                        <Icon name="barbell" />
+                        <Icon.Button name="barbell">
+                                Cipolla
+                        </Icon.Button>
+                </View>
+        );
+}
+
+const AppNavigator: React.FunctionComponent = () => {
+        return (
+                <NavigationContainer>
+                        <Tab.Navigator
+                                backBehavior="history"
+                                barStyle={{
+                                        backgroundColor: 'royalblue',
+                                }}
+                                initialRouteName="Home">
+                                <Tab.Screen
+                                        name={i18next.t('nav.home')}
+                                        options={{
+                                                tabBarIcon: TabIconHome,
+                                        }}
+                                        component={HomeScreen}
+                                />
+                                <Tab.Screen
+                                        name={i18next.t('nav.5qcard')}
+                                        options={{
+                                                tabBarIcon: TabIcon5Q,
+                                        }}
+                                        component={HomeScreen}
+                                />
+                                <Tab.Screen
+                                        name={i18next.t('nav.kts')}
+                                        options={{
+                                                tabBarIcon: TabIconKTS,
+                                        }}
+                                        component={HomeScreen}
+                                />
+                                <Tab.Screen
+                                        name={i18next.t('nav.tkc')}
+                                        options={{
+                                                tabBarIcon: TabIconTKC,
+                                        }}
+                                        component={HomeScreen}
+                                />
+                                <Tab.Screen
+                                        name={i18next.t('nav.about')}
+                                        component={HomeScreen}
+                                        options={{
+                                                tabBarIcon: TabIconAbout,
+                                        }}
+                                />
+                        </Tab.Navigator>
+                </NavigationContainer>
+        );
+};
+
+const customFonts = {
+        OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+        OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+        OpenSansItalic: require('./assets/fonts/OpenSans-Italic.ttf'),
+};
+
+const App: React.FunctionComponent = () => {
+        return <AppNavigator />;
+};
 
 export default App;

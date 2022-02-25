48295a9cb01ab2b6a8eca573b71740621b7d9564
Marco Bresciani <>
2022-02-25 18:27:00 +0100

    Rework copyright page

--- app/screens/about/Copyright.tsx
+++ app/screens/about/Copyright.tsx
@@ -21,11 +21,12 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 import * as React from 'react';
-import {DefaultText} from '../../components/Texts';
+import {DefaultText, SmallerText} from '../../components/Texts';
 import styled from 'styled-components';
-import {ScrollView} from 'react-native';
+import {Linking, ScrollView, View} from 'react-native';
+import {BrowseButton} from '../../components/Buttons';
 
-const Notice = styled(DefaultText)`
+const Notice = styled(View)`
         align-content: center;
         border: 1px solid black;
         border-radius: 10px;
@@ -35,29 +36,56 @@ const Notice = styled(DefaultText)`
 const Copyright: React.FunctionComponent = () => {
         return (
                 <ScrollView>
-                        <Notice>© 2021-2022 Marco Bresciani</Notice>
                         <Notice>
-                                TKCompanionApp is free software: you can
-                                redistribute it and/or modify it under
-                                the terms of the GNU General Public
-                                License as published by the Free
-                                Software Foundation, either version 3 of
-                                the License, or (at your option) any
-                                later version.
-                                {'\n'}
-                                TKCompanionApp is distributed in the
-                                hope that it will be useful, but WITHOUT
-                                ANY WARRANTY; without even the implied
-                                warranty of MERCHANTABILITY or FITNESS
-                                FOR A PARTICULAR PURPOSE. See the GNU
-                                General Public License for more details.
-                                {'\n'}
-                                You should have received a copy of the
-                                GNU General Public License along with
-                                TKCompanionApp. If not, see
-                                &lt;https://www.gnu.org/licenses/&gt;.
+                                <DefaultText>
+                                        © 2021-2022 Marco Bresciani
+                                </DefaultText>
+                                <SmallerText>
+                                        TKCompanionApp is free software:
+                                        you can redistribute it and/or
+                                        modify it under the terms of the
+                                        GNU General Public License as
+                                        published by the Free Software
+                                        Foundation, either version 3 of
+                                        the License, or (at your option)
+                                        any later version.
+                                </SmallerText>
+                                <SmallerText>
+                                        TKCompanionApp is distributed in
+                                        the hope that it will be useful,
+                                        but WITHOUT ANY WARRANTY;
+                                        without even the implied
+                                        warranty of MERCHANTABILITY or
+                                        FITNESS FOR A PARTICULAR
+                                        PURPOSE. See the GNU General
+                                        Public License for more details.
+                                </SmallerText>
+                                <SmallerText>
+                                        You should have received a copy
+                                        of the GNU General Public
+                                        License along with
+                                        TKCompanionApp. If not, see
+                                        &lt;https://www.gnu.org/licenses/&gt;.
+                                </SmallerText>
                         </Notice>
-                        <Notice>Open Sans © 2021 Steve Matteson</Notice>
+                        <Notice>
+                                <DefaultText>
+                                        FreeSans, GNU FreeFont
+                                </DefaultText>
+                                <DefaultText>
+                                        © 2012 Primož Peterlin, Steve
+                                        White
+                                </DefaultText>
+                                <BrowseButton
+                                        onPress={() => {
+                                                Linking.openURL(
+                                                        'https://www.gnu.org/software/freefont/license.html',
+                                                );
+                                        }}
+                                        text="See license page"
+                                        theme={{bg: '#880088'}}
+                                />
+                        </Notice>
                 </ScrollView>
         );
 };

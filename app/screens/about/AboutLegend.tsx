ecdaa8f31530774e1490fc46eb6f1f6fd38ca509
Marco Bresciani <>
2022-03-03 08:00:18 +0100

    Rework About screen
    Ready to add Legend.

---
+++ app/screens/about/AboutLegend.tsx
@@ -0,0 +1,30 @@
+/*
+ * © 2021-2022 Marco Bresciani
+ * This file is part of TKCompanionApp.
+ *
+ * TKCompanionApp is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * TKCompanionApp is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TKCompanionApp.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-FileCopyrightText: 2021-2022 Marco Bresciani
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+import * as React from 'react';
+import DefaultScreen from '../DefaultScreen';
+
+const AboutLegend: React.FunctionComponent = () => {
+        return <DefaultScreen></DefaultScreen>;
+};
+
+export default AboutLegend;

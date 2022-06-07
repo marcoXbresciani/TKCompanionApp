1bb369663bb1eac7f3fca902d60b62298cc72f7d
Marco Bresciani <>
2022-06-07 10:11:44 +0200

    Update project configuration
    See about Java 11 in F-Droid.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,19 @@
 module.exports = {
-    root: true,
-    extends: ['@react-native-community', 'eslint-config-prettier'],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        'prettier',
-        'react-native'
-    ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        jsx: true,
-        tsconfigRootDir: __dirname,
-        project: './tsconfig.json'
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+      'linebreak-style': ['error', 'windows'],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
     },
-    rules: {
-        'linebreak-style': ['error', 'windows'],
-    },
-    overrides: [
-        {
-            files: ['*.ts', '*.tsx'],
-            rules: {
-                '@typescript-eslint/no-shadow': ['error'],
-                'no-shadow': 'off',
-                'no-undef': 'off',
-            },
-        },
-    ],
+  ],
 };

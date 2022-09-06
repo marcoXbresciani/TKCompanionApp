d1640fcd66325c5a84d91bd96794c77ab9c3fa39
Marco Bresciani <>
2022-09-06 10:17:41 +0200

    Reformat text

--- app/pages/home/Copyright.tsx
+++ app/pages/home/Copyright.tsx
@@ -50,23 +50,22 @@ const Copyright: React.FunctionComponent = () => {
       <Notice>
         <TkText>© 2021-2022 Marco Bresciani</TkText>
         <SmallerText>
-          TKCompanionApp is free software: you can
-          redistribute it and/or modify it under the terms of
-          the GNU General Public License as published by the
-          Free Software Foundation, either version 3 of the
-          License, or (at your option) any later version.
+          TKCompanionApp is free software: you can redistribute it
+          and/or modify it under the terms of the GNU General Public
+          License as published by the Free Software Foundation, either
+          version 3 of the License, or (at your option) any later
+          version.
         </SmallerText>
         <SmallerText>
-          TKCompanionApp is distributed in the hope that it
-          will be useful, but WITHOUT ANY WARRANTY; without
-          even the implied warranty of MERCHANTABILITY or
-          FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-          General Public License for more details.
+          TKCompanionApp is distributed in the hope that it will be
+          useful, but WITHOUT ANY WARRANTY; without even the implied
+          warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+          PURPOSE. See the GNU General Public License for more details.
         </SmallerText>
         <SmallerText>
-          You should have received a copy of the GNU General
-          Public License along with TKCompanionApp. If not,
-          see &lt;https://www.gnu.org/licenses/&gt;.
+          You should have received a copy of the GNU General Public
+          License along with TKCompanionApp. If not, see
+          &lt;https://www.gnu.org/licenses/&gt;.
         </SmallerText>
       </Notice>
       <Notice>

ce1f6cfdcb254c41a07233e46a83429eb96444a4
Marco Bresciani <marco.bresciani@gucci.com>
2023-04-06 14:34:43 +0200

    Rework and simplify legend page

--- app/pages/home/dialogs/Legend.tsx
+++ app/pages/home/dialogs/Legend.tsx
@@ -26,127 +26,143 @@ import { Dialog, List } from 'react-native-paper'
 import i18n from '../../../i18n/i18n'
 
 const Legend: React.FC = () => {
+  const legendAreas = [
+    {
+      items: [
+        {
+          description: i18n.t('nav.menu.settings.explain'),
+          icon: 'tune-variant',
+          title: i18n.t('nav.menu.settings.title')
+        },
+        {
+          description: i18n.t('nav.menu.legend.explain'),
+          icon: 'help-box',
+          title: i18n.t('nav.menu.legend.title')
+        },
+        {
+          description: i18n.t('nav.menu.contact.explain'),
+          icon: 'comment-quote-outline',
+          title: i18n.t('nav.menu.contact.title')
+        },
+        {
+          description: i18n.t('nav.menu.copyright.explain'),
+          icon: 'copyleft',
+          title: i18n.t('nav.menu.copyright.title')
+        },
+        {
+          description: i18n.t('nav.menu.legend.browse.explain'),
+          icon: 'home-export-outline',
+          title: i18n.t('nav.menu.legend.browse.title')
+        },
+        {
+          description: i18n.t('nav.menu.about.explain'),
+          icon: 'information-variant',
+          title: i18n.t('nav.menu.about.title')
+        }
+      ],
+      title: i18n.t('nav.home')
+    },
+    {
+      items: [
+        {
+          description: i18n.t('pdsa.legend.calendar.explain'),
+          icon: 'calendar-edit',
+          title: i18n.t('pdsa.legend.calendar.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.before.explain'),
+          icon: 'calendar-arrow-left',
+          title: i18n.t('pdsa.legend.before.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.today.explain'),
+          icon: 'calendar-today',
+          title: i18n.t('pdsa.legend.today.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.after.explain'),
+          icon: 'calendar-arrow-right',
+          title: i18n.t('pdsa.legend.after.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.save.explain'),
+          icon: 'content-save-all',
+          title: i18n.t('pdsa.legend.save.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.delete.explain'),
+          icon: 'trash-can-outline',
+          title: i18n.t('pdsa.legend.delete.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.duplicate.explain'),
+          icon: 'content-duplicate',
+          title: i18n.t('pdsa.legend.duplicate.title')
+        },
+        {
+          description: i18n.t('pdsa.legend.empty.explain'),
+          icon: 'trash-can',
+          title: i18n.t('pdsa.legend.empty.title')
+        }
+      ],
+      title: i18n.t('pdsa.title')
+    },
+    {
+      items: [
+        {
+          description: i18n.t('docs.legend.read.explain'),
+          icon: 'page-next-outline',
+          title: i18n.t('docs.legend.read.title')
+        },
+        {
+          description: i18n.t('docs.legend.download.explain'),
+          icon: 'tray-arrow-down',
+          title: i18n.t('docs.legend.download.title')
+        },
+        {
+          description: i18n.t('docs.legend.browse.explain'),
+          icon: 'home-export-outline',
+          title: i18n.t('docs.legend.browse.title')
+        }
+      ],
+      title: i18n.t('docs.title')
+    },
+    {
+      items: [
+        {
+          description: i18n.t('events.legend.browse.explain'),
+          icon: 'home-export-outline',
+          title: i18n.t('events.legend.browse.title')
+        }
+      ],
+      title: i18n.t('events.title')
+    }
+  ]
+
   return (
     <>
       <Dialog.Title>{i18n.t('nav.menu.legend.title')}</Dialog.Title>
       <ScrollView>
         <Dialog.Content>
-          <List.Accordion title={i18n.t('nav.home')}>
-            <List.Item
-              left={props => <List.Icon {...props} icon='tune-variant' />}
-              title={i18n.t('nav.menu.settings.title')}
-              description={i18n.t('nav.menu.settings.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='help-box' />}
-              title={i18n.t('nav.menu.legend.title')}
-              description={i18n.t('nav.menu.legend.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='comment-quote-outline' />}
-              title={i18n.t('nav.menu.contact.title')}
-              description={i18n.t('nav.menu.contact.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='copyleft' />}
-              title={i18n.t('nav.menu.copyright.title')}
-              description={i18n.t('nav.menu.copyright.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='home-export-outline' />}
-              title={i18n.t('nav.menu.legend.browse.title')}
-              description={i18n.t('nav.menu.legend.browse.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='information-variant' />}
-              title={i18n.t('nav.menu.about.title')}
-              description={i18n.t('nav.menu.about.explain')}
-              descriptionNumberOfLines={3}
-            />
-          </List.Accordion>
-          <List.Accordion title={i18n.t('pdsa.title')}>
-            <List.Item
-              left={props => <List.Icon {...props} icon='calendar-edit' />}
-              title={i18n.t('pdsa.legend.calendar.title')}
-              description={i18n.t('pdsa.legend.calendar.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='calendar-arrow-left' />}
-              title={i18n.t('pdsa.legend.before.title')}
-              description={i18n.t('pdsa.legend.before.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='calendar-today' />}
-              title={i18n.t('pdsa.legend.today.title')}
-              description={i18n.t('pdsa.legend.today.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='calendar-arrow-right' />}
-              title={i18n.t('pdsa.legend.after.title')}
-              description={i18n.t('pdsa.legend.after.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='content-save-all' />}
-              title={i18n.t('pdsa.legend.save.title')}
-              description={i18n.t('pdsa.legend.save.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='trash-can-outline' />}
-              title={i18n.t('pdsa.legend.delete.title')}
-              description={i18n.t('pdsa.legend.delete.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='content-duplicate' />}
-              title={i18n.t('pdsa.legend.duplicate.title')}
-              description={i18n.t('pdsa.legend.duplicate.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='trash-can' />}
-              title={i18n.t('pdsa.legend.empty.title')}
-              description={i18n.t('pdsa.legend.empty.explain')}
-              descriptionNumberOfLines={3}
-            />
-          </List.Accordion>
-          <List.Accordion title={i18n.t('docs.title')}>
-            <List.Item
-              left={props => <List.Icon {...props} icon='page-next-outline' />}
-              title={i18n.t('docs.legend.read.title')}
-              description={i18n.t('docs.legend.read.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='tray-arrow-down' />}
-              title={i18n.t('docs.legend.download.title')}
-              description={i18n.t('docs.legend.download.explain')}
-              descriptionNumberOfLines={3}
-            />
-            <List.Item
-              left={props => <List.Icon {...props} icon='home-export-outline' />}
-              title={i18n.t('docs.legend.browse.title')}
-              description={i18n.t('docs.legend.browse.explain')}
-              descriptionNumberOfLines={3}
-            />
-          </List.Accordion>
-          <List.Accordion title={i18n.t('events.title')}>
-            <List.Item
-              left={props => <List.Icon {...props} icon='home-export-outline' />}
-              title={i18n.t('events.legend.browse.title')}
-              description={i18n.t('events.legend.browse.explain')}
-              descriptionNumberOfLines={3}
-            />
-          </List.Accordion>
+          <List.AccordionGroup>
+            {legendAreas.flatMap(({ items, title }) => {
+              return (
+                <List.Accordion id={title} title={title}>
+                  {items.flatMap(({ description, icon, title }) => {
+                    return (
+                      <List.Item
+                        description={description}
+                        descriptionNumberOfLines={3}
+                        key={title}
+                        left={props => <List.Icon {...props} icon={icon} />}
+                        title={title}
+                      />
+                    )
+                  })}
+                </List.Accordion>
+              )
+            })}
+          </List.AccordionGroup>
         </Dialog.Content>
       </ScrollView>
     </>

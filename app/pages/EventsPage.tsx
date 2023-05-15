bc03c85da8fee05f972edef14679c9cfe30d2450
Marco Bresciani <marco.bresciani@gucci.com>
2023-05-15 13:52:34 +0200

    Update events

--- app/pages/EventsPage.tsx
+++ app/pages/EventsPage.tsx
@@ -51,16 +51,16 @@ const BadgesPage: React.FC = () => {
       url: 'https://polarismep.org/event/ask-me-anything-toyota-kata/'
     },
     {
-      date: '2023-06-20',
-      location: 'WWW',
-      title: 'Improvement Kata/Coaching Kata Remotely',
-      url: 'https://us02web.zoom.us/webinar/register/WN_-hb9opX0Q9O4D_ieblfo6g'
-    },
-    {
       date: '2023-06-19',
       location: 'US',
       title: 'Kata School Cascadia\'s Kata Together June 19 -23 2023 at Kaas Tailored',
       url: 'https://kscskatatogether2023.eventbrite.com/'
+    },
+    {
+      date: '2023-06-20',
+      location: 'WWW',
+      title: 'Improvement Kata/Coaching Kata Remotely',
+      url: 'https://us02web.zoom.us/webinar/register/WN_-hb9opX0Q9O4D_ieblfo6g'
     },
     {
       date: '2023-07-22',
